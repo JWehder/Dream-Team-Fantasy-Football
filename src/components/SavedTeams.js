afe143ff5b92c95937ddfa89bcebda2a6f4a2f73
Jake Wehder <jw@email.comjake>
2022-11-06 15:44:46 -0600

    added the ability to see saved team names

--- src/components/SavedTeams.js
+++ src/components/SavedTeams.js
@@ -1,20 +1,40 @@
 import React, {useState, useEffect} from "react";
+import { Redirect } from "react-router-dom"
 
-function SavedTeams() {
+function SavedTeams({ isLoggedIn }) {
 
     const [savedTeams, setSavedTeams] = useState([])
 
     useEffect(() => {
         fetch('http://localhost:3000/teams')
         .then(resp => resp.json())
-        .then((team) => setSavedTeams(team))
+        .then((teams) => teams.map((teamObj) => {
+            const teamKeys = Object.keys(teamObj)
+            let teamName = teamKeys[0]
+            setSavedTeams([
+                ...savedTeams,
+                {
+                    id: teamObj.id,
+                    myPlayers: teamObj["myPlayers"],
+                    teamName: teamName
+                }
+            ])
+        }))
     }, []); 
 
-    
+    const savedTeamsToDisplay = savedTeams.map((savedTeam) => {
+        return <li key= {savedTeam.id}>{savedTeam.teamName}</li>
+    })
 
+
+    if (!isLoggedIn) return <Redirect to="/login" />
     return (
-        <h1>creating functionality...</h1>
-
+        <div>
+            <h1>creating functionality...</h1>
+            <ul>
+                {savedTeamsToDisplay}
+            </ul>
+        </div>
     )
 }
 
